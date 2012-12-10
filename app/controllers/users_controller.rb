93e102e46a4450199e954e69024dd16a20d06edf
Erik Krietsch <ekrietsch@ezlinks.com>
2012-12-10 10:09:27 -0600

    Testing :title symbol

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def home
+    
     @user = User.find_by_auth_token(session[:uid])
     @babies = @user.babies
+    :title = @user.display_name
   end
   
   def index
